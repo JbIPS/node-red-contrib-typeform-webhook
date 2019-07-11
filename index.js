175e51e43663176e87bec715336cfe8d2b98dd7e
Jean-Baptiste Richardet <jeanbaptiste.richardet@gmail.com>
2019-07-11 15:35:48 +0200

    Adds hidden fields to the payload

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ module.exports = function(RED) {
 		RED.nodes.createNode(this,config);
 		RED.httpNode.post(config.path, (req, res) => {
 			const {body} = req;
-			const payload = body.form_response.answers.reduce((memo, ans) => {
-				let value = ans[ans.type];
-				if(ans.type === 'choice'){
-					value = value.label;
-				} else if(ans.type === 'choices'){
-					value = value.labels;
-				}
-				memo[ans.field.ref] = value;
-				return memo
-			}, {});
+			const payload = {
+				answers: body.form_response.answers.reduce((memo, ans) => {
+					let value = ans[ans.type];
+					if(ans.type === 'choice'){
+						value = value.label;
+					} else if(ans.type === 'choices'){
+						value = value.labels;
+					}
+					memo[ans.field.ref] = value;
+					return memo
+				}, {}),
+				hidden: body.form_response.hidden
+			};
 			this.send({payload});
 			return res.send('OK');
 		});
